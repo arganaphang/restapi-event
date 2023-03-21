f63e161a401d1d6721ece7e2f06ed3a61c95c39f
Argana Phangquestian <arganaphangquestian@gmail.com>
2023-03-22 01:42:22 +0700

    add: schema update: extract postgres container

---
+++ database/initdb.sql
@@ -0,0 +1,12 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+DROP TABLE IF EXISTS "transactions";
+
+CREATE TABLE IF NOT EXISTS "transactions"(
+  "transaction_id" UUID PRIMARY KEY,
+  "id" BIGINT NOT NULL,
+  "customer" VARCHAR NOT NULL,
+  "quantity" SMALLINT NOT NULL,
+  "price" DECIMAL(10, 2) NOT NULL,
+  "timestamp" TIMESTAMP NOT NULL
+);
